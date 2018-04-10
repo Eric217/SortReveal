753d31f31fe89edf6eb9079ceb7c71f15402ce38
liu <erisl@icloud.com>
2018-04-10 11:01:25 +0800

    first commit

---
+++ SortReveal/SortReveal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortReveal
+//
+//  Created by Eric on 2018/4/10.
+//  Copyright © 2018 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SortReveal/SortReveal/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SortReveal
+//
+//  Created by Eric on 2018/4/10.
+//  Copyright © 2018 Eric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
